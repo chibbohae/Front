c2b7b2e26205cb58d29480bd069344c060b6a946
HanseKim <kimbird2002@naver.com>
2025-03-17 15:43:53 +0900

    3/17 url 적용

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:3000", // 로컬 대체 주소
   headers: {
     "Content-Type": "application/json",
   },
