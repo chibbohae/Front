e889a6e2737b80bb853e5969f8a0722ab1ba9b18
HanseKim <kimbird2002@naver.com>
2025-05-12 20:32:58 +0900

    민찬 wss 테스트

--- src/component/CallChat/Calltest.tsx
+++ src/component/CallChat/Calltest.tsx
@@ -53,7 +53,7 @@ const Calltest: React.FC<CalltestProps> = ({ onComplete }) => {
     const apiUrl = 'https://adc9-61-42-109-13.ngrok-free.app' // "http://localhost:5001"; // 다른 CORS 프록시 사용
     
     // WebSocket URL 수정 - Socket.io 경로 형식에 맞게 변경
-    const socketUrl = "ws://chibbohae.link/signaling/ws/test_user"
+    const socketUrl = `wss://chibbohae.link/signaling/ws/${userId}`;
     // "https://3930-36-38-154-98.ngrok-free.app" //"localhost:5001"; // `ws://chibbohae.link/signaling/ws/test_user`;
 
     const startRecording = async (stream: MediaStream) => {
